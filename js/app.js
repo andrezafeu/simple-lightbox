9da902a245ed1be07130ef2c6f8f9fd194b362f6
Andreza Feu <andrezafeu@gmail.com>
2016-06-25 19:02:50 -0400

    Add overlay

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// The $ in front of the overlay is just a convention. Doesn't add anything to the code.
+var $overlay = $('<div id="overlay"></div>');
+$("body").append($overlay);
+
 $("#image-gallery-js a").click(function(event){
 	event.preventDefault();
 });

60b22eb336eb09bcf6f27b17461a1e5cb4b8a622
Andreza Feu <andrezafeu@gmail.com>
2016-06-25 19:14:55 -0400

    Show image on overlay

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 // The $ in front of the overlay is just a convention. Doesn't add anything to the code.
 var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+
+$overlay.append($image)
+
 $("body").append($overlay);
 
 $("#image-gallery-js a").click(function(event){
 	event.preventDefault();
+	var imageLocation = $(this).attr("href");
+	// show image when clicked
+	$image.attr("src", imageLocation);
+	$overlay.show();
 });
